91e35c8f943ca40397804bbd9d60e6e74bc28e1a
Flavio Aurelio de Souza Lima <bhlima2@gmail.com>
2017-12-08 03:40:57 -0200

    Meli adicionado

--- admin/language/pt-br/extension/module/opencartml.php
+++ admin/language/pt-br/extension/module/opencartml.php
@@ -1,40 +1,72 @@
 <?php
+///////////////////////////////////////////////////////////////////////////////
+// Integração OpenCart Mercadolivre - 2017
+// Arquivo de idiomas - Portugues Brasileiro
+// Este arquivo é copia do arquivo na pasta portugues para ser traduzido
+///////////////////////////////////////////////////////////////////////////////
+
+
 
 //Heading
-$_['heading_title']             = 'Opencart Mercadolivre';
+$_['heading_title']                 = 'Opencart Mercadolivre';
 
 //Text
-$_['text_pagseguro']            = '<img src="view/image/payment/pagseguro.png" />';
-$_['text_success']              = 'Módulo atualizado com sucesso.';
+$_['text_module']                   = 'Modulo';
+$_['text_success']                  = 'Módulo atualizado com sucesso.';
+$_['text_ml_used']                  = 'Produto Usado';
+$_['text_ml_new']                   = 'Produto Novo';
+
+
+
 
 //Tab
-$_['tab_config']                = 'Configuração';
-$_['tab_automate']              = 'Feedback automático';
-$_['tab_order_config']          = 'Vendas';
-$_['tab_listing_config']        = 'Listagem padrão';
-$_['tab_template']              = 'Template';
+$_['tab_config']                    = 'Configuração';
+$_['tab_automate']                  = 'Catalogo';
+$_['tab_order_config']              = 'Opções de vendas';
+$_['tab_listing_config']            = 'Anuncio Padrão';
+$_['tab_template']                  = 'Template';
+$_['tab_ml_automation']             = 'Feedback';
+
 
 
 
 //Entry
-$_['entry_status']              = 'Situação:';
-$_['entry_client_id']           = 'App ID:';
-$_['entry_client_secret']       = 'Código Secreto';
-$_['entry_access_token']        = 'Token de acesso:';
-$_['entry_debug']               = 'Debug:';
+$_['entry_status']                  = 'Situação:';
+$_['entry_client_id']               = 'Client ID:';
+$_['entry_client_secret']           = 'Código Secreto';
+$_['entry_debug']                   = 'Debug:';
+$_['entry_ml_category']             = 'Categoria:';
+$_['entry_ml_subcategory']          = 'Sub-categoria:';
+$_['entry_ml_currency']             = 'Moeda:';
+$_['entry_ml_adtype']               = 'Tipo:';
+$_['entry_ml_itemcondition']        = 'Estado:';
+$_['entry_ml_automation']           = 'Tarefas automaticas';
+$_['entry_ml_feedback_status']      = 'Situação';
+$_['entry_ml_feedback_status_post'] = 'Qualificar quando';
+$_['entry_ml_template_default']     = 'Descrição';
+$_['entry_ml_feedback_rating']      = 'Qualificação';
 
 //Help
-$_['help_status'] = 'Habilite ou Desabilite o módulo (Essa opção não habilitará/desabilitará os métodos de pagamento)';
-$_['help_client_id'] = 'Token de Segurança. Caso não tenha entre em contato com o suporte do PagSeguro';
-$_['help_client_secret'] = 'É obrigatório que a CONTA TESTE seja qualquer-coisa@SANDBOX.PAGSEGURO.COM.BR';
-$_['help_redirect_uri'] = 'É obrigatório que a CONTA TESTE seja qualquer-coisa@SANDBOX.PAGSEGURO.COM.BR';
-$_['help_access_token'] = 'É obrigatório que a CONTA TESTE seja qualquer-coisa@SANDBOX.PAGSEGURO.COM.BR';
-$_['help_debug'] = 'Salva em um arquivo de log todos os dados das requisições';
+$_['help_status']                   = 'Habilite ou Desabilite o módulo Mercado Livre)';
+$_['help_client_id']                = 'APP Id você obtem acessando https://developers.mercadolivre.com.br/apps/home';
+$_['help_client_secret']            = 'E a contra senha você obtem acessando https://developers.mercadolivre.com.br/apps/home';
+$_['help_redirect_uri']             = 'Falta testo';
+$_['help_access_token']             = 'Falta testo';
+$_['help_debug']                    = 'Salva em um arquivo de log todos os dados das requisições';
+$_['help_ml_category']              = 'Categoria principal na listagem do mercadolivre';
+$_['help_ml_sub_category']          = 'Sub categoria na listagem do mercadolivre';
+$_['help_ml_currency']              = 'Moeda padrão dos seus anuncios';
+$_['help_ml_adtype']                = 'Selecione a quantide extra de aparições';
+$_['help_ml_itemcondition']         = 'Especifique o que você vai vender se é novo ou usado';
+$_['help_ml_feedback_status']       = 'Se estiver ligado, qualifica os clientes assim que o produto é enviado';
+$_['help_ml_feedback_status_post']  = 'Determina quando o indivíduo deve ser qualificado';
+$_['help_ml_template_default']      = 'Se estiver ligado, qualifica os clientes assim que o produto é enviado';
+$_['help_ml_feedback_rating']       = 'Qualificação que você vai postar automaticamente';
 
 
 //Error
-$_['warning'] = 'Você não tem permissão para modificar esse módulo';
-$_['error_client_id'] = 'ID do client inválido';
-$_['error_client_secret'] = 'Token Inválido';
-$_['error_redirect_uri'] = 'Url de retorno invalida';
-$_['error_access_token'] = 'Token Inválido';
+$_['warning']                       = 'Você não tem permissão para modificar esse módulo';
+$_['error_client_id']               = 'App ID não pode ser verificada';
+$_['error_client_secret']           = 'Codigo secreto falso';
+$_['error_redirect_uri']            = 'Informação inconsistente, esta urls não responde';
+$_['error_access_token']            = 'Token não é válido - Verique em sua aplicação';
